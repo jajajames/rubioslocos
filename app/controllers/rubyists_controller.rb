5cd262435c825c63a272a19fac71ac07fa3ac962
J-Henry <JH@JH.local>
2016-04-20 19:29:34 -0400

    Add user info: join, repo#, location

--- app/controllers/rubyists_controller.rb
+++ app/controllers/rubyists_controller.rb
@@ -2,8 +2,20 @@ class RubyistsController < ApplicationController
 
   def index
     query = "location:Indianapolis language:Ruby"
-    options = { per_page:9999, order:"asc", sort:"joined" }
-    lrs = Octokit.search_users(query, options)
-    @locosrubios = lrs.items
+    options = { per_page:9999, order:"desc", sort:"joined" }
+    lrs = Octokit.search_users(query, options).items
+    @locosrubios = map_gh_info(lrs)
+  end
+
+  private
+
+  def map_gh_info(users)
+    users.collect do |lr|
+      user = Octokit.user lr.username
+      lr.created_at   = human_time(user.created_at)
+      lr.public_repos = user.public_repos
+      lr.location     = user.location
+    end
+    users
   end
 end

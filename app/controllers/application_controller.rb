4b9e5f60f7721a847d347c487cd01eda3094de04
J-Henry <JH@JH.local>
2016-04-20 22:21:17 -0400

    Correct GitHub user index data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  private
-
-  def human_time(time)
-    time.to_date.to_s.humanize.slice(2,9)
-  end
 end
